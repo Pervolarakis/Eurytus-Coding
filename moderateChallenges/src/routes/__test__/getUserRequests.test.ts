92d34459c2c6036fa977740c72899e7c669556ec
pervolarakis <pervolarakes@gmail.com>
2021-11-03 10:47:32 +0200

    get my requests now only returns latest request for each challenge id

--- moderateChallenges/src/routes/__test__/getUserRequests.test.ts
+++ moderateChallenges/src/routes/__test__/getUserRequests.test.ts
@@ -67,3 +67,107 @@ it('successfully returns user requests 2', async()=>{
         .expect(200)
     expect(result.body.data).toHaveLength(0);
 })
+
+it('successfully returns user requests 3', async()=>{
+    const user = new mongoose.Types.ObjectId();
+    const date = new Date();
+    const challengeId = new mongoose.Types.ObjectId();
+    date.setDate(date.getDate()+1)
+    const date2 = new Date();
+    date2.setDate(date2.getDate()+5)
+    const request1 = new PendingRequest({
+        kind: 'update',
+        ownerId: user,
+        challengeId: challengeId,
+        data: JSON.stringify({
+            name: "Multiply Challenge2",
+            description: "Write a challenge that multiplies 99 numbers"
+        }),
+        message: 'update my challenge',
+        created_at: date.toISOString(),
+    })
+    const request2 = new PendingRequest({
+        kind: 'update',
+        ownerId: user,
+        challengeId: challengeId,
+        data: JSON.stringify({
+            name: "Multiply Challenge2",
+            description: "Write a challenge that multiplies 160 numbers"
+        }),
+        message: 'update my challenge',
+        created_at: new Date().toISOString(),
+    })
+    const request3 = new PendingRequest({
+        kind: 'update',
+        ownerId: user,
+        challengeId: challengeId,
+        data: JSON.stringify({
+            name: "Multiply Challenge2",
+            description: "Write a challenge that multiplies 667 numbers"
+        }),
+        message: 'update my challenge',
+        created_at: date2.toISOString(),
+    })
+    request1.save();
+    request2.save();
+    request3.save();
+    const result = await request(app)
+        .get(`/api/v1/moderate/myrequests`)
+        .set('Cookie', global.signin(user, 'admin'))
+        .expect(200)
+    expect(result.body.data.length).toEqual(1)
+    expect(result.body.data).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({data: JSON.stringify({
+                name: "Multiply Challenge2",
+                description: "Write a challenge that multiplies 667 numbers"
+            })})
+        ])
+    )
+})
+
+it('successfully returns user requests 4', async()=>{
+    const user = new mongoose.Types.ObjectId();
+    const date = new Date();
+    const challengeId = new mongoose.Types.ObjectId();
+    date.setDate(date.getDate()+1)
+    const date2 = new Date();
+    date2.setDate(date2.getDate()+5)
+    const request1 = new PendingRequest({
+        kind: 'update',
+        ownerId: user,
+        challengeId: challengeId,
+        data: JSON.stringify({
+            name: "Multiply Challenge2",
+            description: "Write a challenge that multiplies 99 numbers"
+        }),
+        message: 'update my challenge',
+        created_at: date.toISOString(),
+    })
+    const request2 = new PendingRequest({
+        kind: 'update',
+        ownerId: user,
+        challengeId: challengeId,
+        data: JSON.stringify({
+            name: "Multiply Challenge2",
+            description: "Write a challenge that multiplies 160 numbers"
+        }),
+        message: 'update my challenge',
+        created_at: new Date().toISOString(),
+    })
+    request1.save();
+    request2.save();
+    const result = await request(app)
+        .get(`/api/v1/moderate/myrequests`)
+        .set('Cookie', global.signin(user, 'admin'))
+        .expect(200)
+    expect(result.body.data.length).toEqual(1)
+    expect(result.body.data).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({data: JSON.stringify({
+                name: "Multiply Challenge2",
+                description: "Write a challenge that multiplies 99 numbers"
+            })})
+        ])
+    )
+})
