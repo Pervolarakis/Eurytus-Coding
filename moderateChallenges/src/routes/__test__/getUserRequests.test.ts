449e786e4845965d48e3b440cc3d6f1e646af615
pervolarakis <pervolarakes@gmail.com>
2021-11-03 10:51:54 +0200

    added more tests

--- moderateChallenges/src/routes/__test__/getUserRequests.test.ts
+++ moderateChallenges/src/routes/__test__/getUserRequests.test.ts
@@ -171,3 +171,67 @@ it('successfully returns user requests 4', async()=>{
         ])
     )
 })
+
+it('successfully returns user requests 5', async()=>{
+    const user = new mongoose.Types.ObjectId();
+    const date = new Date();
+    const challengeId = new mongoose.Types.ObjectId();
+    date.setDate(date.getDate()+1)
+    const date2 = new Date();
+    date2.setDate(date2.getDate()+5)
+    const request1 = new PendingRequest({
+        kind: 'update',
+        ownerId: user,
+        challengeId: challengeId,
+        data: JSON.stringify({
+            name: "Multiply Challenge2",
+            description: "Write a challenge that multiplies 99 numbers"
+        }),
+        message: 'update my challenge',
+        created_at: date.toISOString(),
+    })
+    const request2 = new PendingRequest({
+        kind: 'update',
+        ownerId: user,
+        challengeId: challengeId,
+        data: JSON.stringify({
+            name: "Multiply Challenge2",
+            description: "Write a challenge that multiplies 160 numbers"
+        }),
+        message: 'update my challenge',
+        created_at: new Date().toISOString(),
+    })
+    const request3 = new PendingRequest({
+        kind: 'update',
+        ownerId: user,
+        challengeId: new mongoose.Types.ObjectId(),
+        data: JSON.stringify({
+            name: "Multiply Challenge2",
+            description: "Write a challenge that multiplies 667 numbers"
+        }),
+        message: 'update my challenge',
+        created_at: date2.toISOString(),
+    })
+    request1.save();
+    request2.save();
+    request3.save();
+    const result = await request(app)
+        .get(`/api/v1/moderate/myrequests`)
+        .set('Cookie', global.signin(user, 'admin'))
+        .expect(200)
+    expect(result.body.data.length).toEqual(2)
+    expect(result.body.data).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({data: JSON.stringify({
+                name: "Multiply Challenge2",
+                description: "Write a challenge that multiplies 667 numbers"
+            })}),
+            expect.objectContaining({data: JSON.stringify({
+                name: "Multiply Challenge2",
+                description: "Write a challenge that multiplies 99 numbers"
+            })})
+        ])
+        
+            
+    )
+})
