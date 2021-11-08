cb87cf0538cbe590f5826d20241ebc17ee74d016
pervolarakis <pervolarakes@gmail.com>
2021-11-08 12:22:15 +0200

    added filtering when getting challenges to exclude sensitive fields if user doesnt own the challenge or if user isnt an admin

--- challenges/src/routes/getAllChallenges.ts
+++ challenges/src/routes/getAllChallenges.ts
@@ -7,11 +7,11 @@ const router = express.Router();
 router.get('/api/v1/challenges/',async(req,res,next)=>{
     try{
         if(req.query.language){
-            const challenges = await Challenge.find({isPublic: true, status: 'approved', ...req.query});
+            const challenges = await Challenge.find({isPublic: true, status: 'approved', ...req.query}).select(['-expectedOutputTests', '-expectedStructure', '-expectedDesignPatterns']);
             res.status(200).json({success: true, data: challenges})
             return next();
         }
-        const challenges = await Challenge.find({isPublic: true, status: 'approved'});
+        const challenges = await Challenge.find({isPublic: true, status: 'approved'}).select(['-expectedOutputTests', '-expectedStructure', '-expectedDesignPatterns']);
         res.status(200).json({success: true, data: challenges})
     }catch(err){
         return next(new BasicCustomError(err, 400));
---
+++ challenges/src/routes/getChallengeById.ts
@@ -0,0 +1,23 @@
+import { Challenge } from "../models/challengeModel";
+import express from 'express';
+import { BasicCustomError, requireAuth } from "@eurytus/common";
+
+const router = express.Router();
+
+router.get('/api/v1/challenges/:id', requireAuth, async(req,res,next)=>{
+    
+    try{
+        const challenges = await Challenge.findById(req.params.id);
+        if(req.currentUser?.role!=='admin' && req.currentUser?.id!==challenges?.creatorId){
+            const challenges = await Challenge.findById(req.params.id).select(['-expectedOutputTests', '-expectedStructure', '-expectedDesignPatterns'])
+            res.status(200).json({success: true, data: challenges})
+        }else{
+            res.status(200).json({success: true, data: challenges})
+        }
+        
+    }catch(err){
+        return next(new BasicCustomError(err, 400));
+    }
+})
+
+export {router as getChallengeById}
