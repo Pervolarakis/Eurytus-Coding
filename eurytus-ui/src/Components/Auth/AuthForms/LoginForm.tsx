86fbd89ccb3556a3259f3ae792969e609c64dcea
Michalis <pervolarakes@gmail.com>
2022-02-13 22:21:56 +0200

    changed favicon, changed auth error messages and added delete private challenge confirmation

--- eurytus-ui/src/Components/Auth/AuthForms/LoginForm.tsx
+++ eurytus-ui/src/Components/Auth/AuthForms/LoginForm.tsx
@@ -24,7 +24,9 @@ const LoginForm = () => {
             .then((res)=>{setUser(res.data.data); navigate('/challenges')})
             .catch(err=>toast.error(err.response?.data.error||'There was an error fetching current user!'))
         })
-        .catch(err=>toast.error(err.response?.data.error||'There was an error Logging in!'))
+        .catch(err=>err.response.data.error.map((err:{message: string, field: string})=>{
+            toast.error(err.message);
+        }))
     }
     
     return(
