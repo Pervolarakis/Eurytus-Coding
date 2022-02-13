2eab1449626a8d627b2804119eac810ef71e9e68
Michalis <pervolarakes@gmail.com>
2022-02-14 01:59:36 +0200

    initialized depl for eurytus ui

--- auth/src/routes/logOut.ts
+++ auth/src/routes/logOut.ts
@@ -3,7 +3,7 @@ import express, {Request, Response, NextFunction} from 'express';
 
 const router = express.Router()
 
-router.post('/api/v1/users/auth/logout', requireAuth, asyncHandler(async(req: Request, res: Response, next: NextFunction)=>{
+router.post('/api/v1/users/auth/logout', asyncHandler(async(req: Request, res: Response, next: NextFunction)=>{
     req.session = null;
     res.status(200).json({})
 }))
