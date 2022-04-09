2598fdaa0c8fda3690a3abb7536d1f8a0816840b
Michalis Pervolarakis <pervolarakes@gmail.com>
2022-04-09 18:23:27 +0300

    added confirm password

--- eurytus-ui/src/Components/Auth/AuthForms/RegisterForm.tsx
+++ eurytus-ui/src/Components/Auth/AuthForms/RegisterForm.tsx
@@ -12,24 +12,29 @@ const RegisterForm = () => {
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     let navigate = useNavigate();
 
     const register = () => {
-        axios.post('/users/auth/signup',{
-          email: email,
-          password: password,
-          firstName: firstName, 
-          lastName: lastName,
-        })
-        .then((res)=>{
-          // console.log(res)
-          axios.get('/users/auth/currentuser')
-            .then((res)=>{setUser(res.data.data); navigate('/challenges')})
-            .catch(err=>toast.error(err.response?.data.error||'There was an error fetching current user!'))
-        })
-        .catch(err=>{(typeof err.response.data.error === 'object')?err.response.data.error.map((err:{message: string, field: string})=>
-            toast.error(err.message)
-        ):toast.error(err.response.data.error||'There was an error registering!')})
+        if(password===confirmPassword){
+            axios.post('/users/auth/signup',{
+                email: email,
+                password: password,
+                firstName: firstName, 
+                lastName: lastName,
+              })
+              .then((res)=>{
+                // console.log(res)
+                axios.get('/users/auth/currentuser')
+                  .then((res)=>{setUser(res.data.data); navigate('/challenges')})
+                  .catch(err=>toast.error(err.response?.data.error||'There was an error fetching current user!'))
+              })
+              .catch(err=>{(typeof err.response.data.error === 'object')?err.response.data.error.map((err:{message: string, field: string})=>
+                  toast.error(err.message)
+              ):toast.error(err.response.data.error||'There was an error registering!')})
+        }else{
+            toast.error(`Passwords don't match!`)
+        }
     }
 
     return (
@@ -39,7 +44,7 @@ const RegisterForm = () => {
             <FloatingLabel name="Last Name" type="text" value={lastName} onChange={(val)=>setLastName(val)}/>
             <FloatingLabel name="Email" type="email" value={email} onChange={(val)=>setEmail(val)}/>
             <FloatingLabel name="Password" type="password" value={password} onChange={(val)=>setPassword(val)}/>
-            <FloatingLabel name="Confirm Password" type="password" value={password} onChange={(val)=>setPassword(val)}/>
+            <FloatingLabel name="Confirm Password" type="password" value={confirmPassword} onChange={(val)=>setConfirmPassword(val)}/>
             <div className="w-full flex justify-center mt-4">
                 <input type="submit" className="rounded-2xl border-solid bg-secondary text-white font-bold uppercase border py-3 px-11 md:w-6/12 transform transition duration-250 hover:scale-110" value="Register" />
             </div>
